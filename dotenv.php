7d5fd87903d93f11e0779045710295c7756a6ccd
Alena Holligan <alena@lumenlearning.com>
2020-02-08 09:54:53 -0500

    don't load in production. show phpinfo

--- dotenv.php
+++ dotenv.php
@@ -2,14 +2,15 @@
 require __DIR__ . '/vendor/autoload.php';
 
 //Immutability refers to if Dotenv is allowed to overwrite existing environment variables. If you want Dotenv to overwrite existing environment variables, use createMutable instead of createImmutable:
-//$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
+$dotenv = Dotenv\Dotenv::createMutable(__DIR__);
+//$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+if (getenv('CONFIG_ENV') != "production") {
+    $dotenv->load();
+}
 $dotenv->required(['DB_SERVER', 'DB_NAME', 'DB_USERNAME', 'DB_PASSWORD']);
 
 header("Content-Type: text/plain");
 var_dump(getenv('S3_BUCKET'));
-//phpinfo();
 
 try {
     $db = new PDO("mysql:host=".getenv('DB_SERVER').";dbname=".getenv('DB_NAME'), getenv('DB_USERNAME'), getenv('DB_PASSWORD'));
